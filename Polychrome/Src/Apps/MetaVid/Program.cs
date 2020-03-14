e66290a234f43e0f8fc47de355f10545f4f0b4ad
SuperValou <valentin.esmieu@tuta.io>
2020-03-14 19:27:36 +0100

    Rename TmdbCrawler app and add MetaVid

---
+++ Polychrome/Src/Apps/MetaVid/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MetaVid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
